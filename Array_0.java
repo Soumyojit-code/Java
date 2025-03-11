32d3179febf9567669856b68ff38b34df3714999
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2025-03-12 01:03:25 +0530

    Add files via upload

---
+++ Array_0.java
@@ -0,0 +1,19 @@
+
+public class Array_0
+  {
+      public static void main(String[] args)
+    {
+        int i;
+        int a[]=new int[4];
+          a[0]=1;
+          a[1]=3;
+          a[2]=4;
+          a[3]=5;
+          for(i=0;i<4;i++)
+          System.out.println(a[i]);
+
+
+
+    }
+
+  }
