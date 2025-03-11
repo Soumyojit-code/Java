5796f50284d470157b9c4a9acfcff710eebc5531
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2025-03-12 01:19:28 +0530

    Add files via upload

---
+++ Array_1.java
@@ -0,0 +1,15 @@
+//print the array elements using for-each loop   
+public class Array_1
+  {
+      public static void main(String[] args)
+    {
+        int a[]={22,23,24,25,20};
+          
+          for(int i:a)
+          System.out.println(i);
+
+
+
+    }
+
+  }
