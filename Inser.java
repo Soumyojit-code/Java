54df898e73d0a7b9836b0740a100cfc404c36038
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2026-01-20 00:45:30 +0530

    Add Inser.java to insert 'CORE' into 'JAVA'

---
+++ Inser.java
@@ -0,0 +1,14 @@
+//insert 'core' at index 0 in the string 'JAVA'
+
+public class String3
+
+ {
+    public static void main(String[] args)
+    {
+        StringBuffer s= new StringBuffer("JAVA");
+        s.insert(0,"CORE");
+        System.out.println(s);
+    }
+
+
+ }
