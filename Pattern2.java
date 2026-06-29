c8b32405bc92b45bc904c82de7f2d13889412673
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2026-06-29 14:55:10 +0530

    Add pattern printing logic in Pattern2.java
    Implement a pattern printing program that displays characters in a specific format.

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+//a b c d e f g
+//  a b c d e
+//    a b c
+//      a
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i * 2; j++) {
+                System.out.print(" ");
+            }
+            char ch = 'a';
+            int totalChars = 2 * (n - i) - 1;
+            for (int j = 0; j < totalChars; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
