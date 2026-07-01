3c82dace2fca55cc733794ef77171eba71b4d81f
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2026-07-01 12:29:50 +0530

    Fix inner loop condition in PairSumCount.java

--- PairSumCount.java
+++ PairSumCount.java
@@ -8,7 +8,7 @@ class PairSumCount {
        int k=6;
        for(int i=0;i<arr.length;i++)
        {
-            for(int j=i+1;j<arr.length;j++)
+            for(int j=i+1;j<arr.length-1;j++)
             {
                 int sum=0;
                 sum=arr[i]+arr[j];
