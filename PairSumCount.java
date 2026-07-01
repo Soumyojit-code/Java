62d0354f8016e4f7e314bfea85e9fda1cd91161b
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2026-07-01 12:29:08 +0530

    Add PairSumCount program to count pairs greater than k
    Implemented a Java program to count pairs with a sum greater than a specified value.

---
+++ PairSumCount.java
@@ -0,0 +1,21 @@
+//write a java program to find the count of pair gater then k;
+
+class PairSumCount {
+    public static void main(String[] args) 
+    {
+       int [] arr={ 5,7,2,3,1};
+       int count=0;
+       int k=6;
+       for(int i=0;i<arr.length;i++)
+       {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                int sum=0;
+                sum=arr[i]+arr[j];
+               if (sum > k)
+                count++;
+            }
+       }
+       System.out.println(count);
+}
+}
