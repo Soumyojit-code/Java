0c31f88c1fd45fb336cd35efc26f1bff936be458
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2026-07-14 22:39:45 +0530

    Implement toLowerCase method in Solution class

---
+++ LeetCode/To-lower-case.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/u/soumyojitbanerjee/
+// 709. To Lower Case
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+        
+    }
+}
